185fb3eec27e3128c544a4d7ac82ee86ead48b9c
Marwan El Sharkawy <35876059+marwan1912@users.noreply.github.com>
2019-10-12 16:35:00 -0500

    Queue

---
+++ Queue.java
@@ -0,0 +1,50 @@
+
+public class Queue {
+
+	LinkedList queue;
+	
+	public Queue() {
+		queue = new LinkedList();
+	}
+	
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+	
+	public int size() {
+		return queue.size();
+	}
+	
+	public void enqueue(int n) {
+		queue.add(n);
+	}
+	
+	public int dequeue() {
+		int n = queue.get(0);
+		queue.remove(0);
+		return n;
+	}
+	
+	public int peek() {
+		return queue.get(0);
+	}
+	
+	public void print() {
+		queue.print();
+	}
+	
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		System.out.println("First out: " + q.dequeue());
+		System.out.println("Peek: " + q.peek());
+		System.out.println("Size: " + q.size());
+		System.out.println("isEmpty: " + q.isEmpty());
+		System.out.println("Secound out: " + q.dequeue());
+		System.out.println("Third out: " + q.dequeue());
+	}
+	
+	
+}
