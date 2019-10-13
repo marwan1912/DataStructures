239107613688d1a87b97b8ca6026d9c8ec6d9a6c
Marwan El Sharkawy <35876059+marwan1912@users.noreply.github.com>
2019-10-12 20:47:51 -0500

    Stack

---
+++ LinkedList.java
@@ -0,0 +1,116 @@
+
+public class LinkedList {
+	
+	Node head;
+	int count;
+	
+	public LinkedList() {
+		head = null;
+		count = 0;
+	}
+	
+	public LinkedList(Node head) {	
+		this.head = head;
+		count = 1;
+	}
+	
+	//Adds a node with data in it.
+	public void add(int data) {		
+		Node newHead = new Node(data);
+		if(head == null) {
+			head = newHead;
+		}else {
+			newHead.setNext(head);
+			head = newHead;
+		}
+		
+		count++;		
+	}
+	
+	//Gets data inside a node of a given index. (Indexes start at 0)
+	public int get(int index) {
+		if(index < 0 || head == null || index >= count) {
+			return -1;
+		}
+		
+		Node current = head;
+		for(int i = 0; i < index; i++) {
+			current = current.getNext();
+		}
+		
+		if(current == null) {
+			return -1;
+		}else {
+			return current.getData();	
+		}
+		
+	}
+	
+	//Gives the size of the list.
+	public int size() {
+		return count;
+	}
+	
+	//Tells is if the list is empty.
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	//Removes a node of a given index from the list. (Indexes start at 0)
+	public void remove(int index) {
+		
+		if(index < 0 || index >= size()) {
+			return;
+		}
+		
+		if(index == 0) {
+			head = head.getNext();
+			count--;
+			return;
+		}
+		
+		Node prev = head;
+		Node temp = prev.getNext();
+		for(int i = 0; i < index - 1; i++) {
+			prev = prev.getNext();
+			temp = prev.getNext();
+		}
+			
+			prev.setNext(temp.getNext());
+			count--;
+	}
+	
+}
+
+class Node{
+	
+	Node next;
+	int data;
+	
+	public Node (int data) {		
+		this.data = data;
+		next = null;
+	}
+	
+	public Node(int data, Node next) {		
+		this.data = data;
+		this.next = next;
+	}
+	
+	public void setData(int data) {		
+		this.data = data;
+	}
+	
+	public void setNext(Node next) {		
+		this.next = next;
+	}
+	
+	public int getData() {	
+		return data;
+	}
+	
+	public Node getNext() {		
+		return next;
+	}
+	
+}
---
+++ Stack.java
@@ -0,0 +1,52 @@
+
+public class Stack {
+
+	LinkedList stack;
+	
+	public Stack() {
+		stack = new LinkedList();
+	}
+	
+	public boolean isEmpty() {
+		return stack.isEmpty();
+	}
+	
+	public int size() {
+		return stack.size();
+	}
+	
+	public void push(int n) {
+		stack.add(n);
+	}
+	
+	public int pop() {
+		int n = stack.get(0);
+		stack.remove(0);
+		return n;
+	}
+	
+	public int top() {
+		return stack.get(0);
+	}
+	
+	public void print() {
+		int c = stack.size();
+		for(int i = 0; i < c; i++) {
+			System.out.println(pop());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		System.out.println("First out: " + s.pop());
+		System.out.println("Top: " + s.top());
+		System.out.println("Size: " + s.size());
+		System.out.println("isEmpty: " + s.isEmpty());
+		System.out.println("Secound out: " + s.pop());
+		System.out.println("Third out: " + s.pop());
+	}
+	
+}
